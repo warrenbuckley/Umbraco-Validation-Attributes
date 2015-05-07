d9629e7dcf37d91adb0a82747e62f6806edf1636
cronincr <craig.cronin@merthyr.gov.uk>
2015-05-07 13:26:08 +0100

    Updated umbracostringlength with errormessage assignment

--- Umbraco Validation Attributes/UmbracoValidationAttributes/UmbracoStringLength.cs
+++ Umbraco Validation Attributes/UmbracoValidationAttributes/UmbracoStringLength.cs
@@ -6,15 +6,19 @@ namespace UmbracoValidationAttributes
 {
     public class UmbracoStringLength : StringLengthAttribute, IClientValidatable
     {
+        private readonly string _errorMessageDictionaryKey;
+
 
         public UmbracoStringLength(string errorMessageDictionaryKey, int maximumLength) : base(maximumLength)
         {
-            ErrorMessage = UmbracoValidationHelper.GetDictionaryItem(errorMessageDictionaryKey);
+            _errorMessageDictionaryKey = errorMessageDictionaryKey;
         }
 
 
         public IEnumerable<ModelClientValidationRule> GetClientValidationRules(ModelMetadata metadata, ControllerContext context)
         {
+            ErrorMessage = UmbracoValidationHelper.GetDictionaryItem(_errorMessageDictionaryKey);
+
             var error   = FormatErrorMessage(metadata.DisplayName);
             var rule    = new ModelClientValidationStringLengthRule(error, MinimumLength, MaximumLength);
 
