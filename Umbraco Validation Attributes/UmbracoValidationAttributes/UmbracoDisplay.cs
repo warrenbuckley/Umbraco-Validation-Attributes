e9db92f5925803325ad5b10faf9c6032f22cc544
cronincr <craig.cronin@merthyr.gov.uk>
2015-05-07 13:17:22 +0100

    Updated umbracodisplay error message assignment

--- Umbraco Validation Attributes/UmbracoValidationAttributes/UmbracoDisplay.cs
+++ Umbraco Validation Attributes/UmbracoValidationAttributes/UmbracoDisplay.cs
@@ -6,11 +6,20 @@ namespace UmbracoValidationAttributes
     [AttributeUsage(AttributeTargets.Method | AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Parameter, AllowMultiple = false)]
     public sealed class UmbracoDisplayName : DisplayNameAttribute
     {
+        private readonly string _dictionaryKey;
+
         // This is a positional argument
         public UmbracoDisplayName(string dictionaryKey)
-            : base(UmbracoValidationHelper.UmbracoHelper.GetDictionaryValue(dictionaryKey))
         {
-            
+            _dictionaryKey = dictionaryKey;
+        }
+
+        public override string DisplayName
+        {
+            get
+            {
+                return UmbracoValidationHelper.UmbracoHelper.GetDictionaryValue(_dictionaryKey);
+            }
         }
     }
 }
