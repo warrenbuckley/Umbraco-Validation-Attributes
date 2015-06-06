c16da5ece7d86209881dcd0e169043bcc13b1269
Alan Mac Kenna <alan.mackenna@serveit.ie>
2015-06-07 00:25:56 +0100

    assign dictionary item to ErrorMessage

--- Umbraco Validation Attributes/UmbracoValidationAttributes/UmbracoRegexValidation.cs
+++ Umbraco Validation Attributes/UmbracoValidationAttributes/UmbracoRegexValidation.cs
@@ -17,7 +17,7 @@ namespace UmbracoValidationAttributes
 
         public IEnumerable<ModelClientValidationRule> GetClientValidationRules(ModelMetadata metadata, ControllerContext context)
         {
-            UmbracoValidationHelper.GetDictionaryItem(_errorMessageDictionaryKey);
+            ErrorMessage = UmbracoValidationHelper.GetDictionaryItem(_errorMessageDictionaryKey);
 
             var error           = FormatErrorMessage(metadata.DisplayName);
             var rule            = new ModelClientValidationRule();
